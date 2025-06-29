cb8d3d09e0a10f3c4b370169ae03a655cbffd020
Ayush0095 <aayushsingh7668@gmail.com>
2025-06-29 11:56:41 +0530

    Creating Routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const exp = require('express');
 const app = exp();
 const userModel = require('./models/user');
+const postModel = require('./models/post');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.set('view engine',"ejs");
 app.use(exp.json());
@@ -12,4 +15,75 @@ app.get("/",(req,res)=>{
     res.render('index');
 });
 
+//Creating the User account.
+app.post("/register",async (req,res)=>{
+    let{email,password,age,name ,username} = req.body;
+
+    //Finding user on the basis of email and then storing it to user variable.
+    //Main motive is avoiding the duplicasy of the user.
+    let user = await userModel.findOne({email});
+    if(user) return res.status(500).send("User already there.");
+
+    //Creating user if user is not present by hashing the pasword.
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt,async(err,hash)=>{
+           let user = await userModel.create({
+                username,
+                email,
+                name,
+                age,
+                password :hash
+            });
+            //sending token as a Cookie by wrapping up the data.
+            let token = jwt.sign({
+                email:email,
+                userid:user._id},
+                "secretKey"
+            )
+            res.cookie("token",token);
+            res.send("User Registered")
+        })
+    })
+});
+
+//Creating Login Page.
+app.get('/login',(req,res)=>{
+    res.render('login');
+})
+
+app.post("/login",async (req,res)=>{
+    let{email,password} = req.body;
+
+    //Finding user on the basis of email.
+    let user = await userModel.findOne({email});
+    if(!user) return res.status(500).send("Something went wrong");
+
+    bcrypt.compare(password,user.password,(err,result)=>{
+        if(result) {
+            let token = jwt.sign({
+                email:email,
+                userid:user._id},
+                "secretKey"
+            )
+             res.status(200).send("You can login")
+        }
+        else res.redirect('/login');
+    })
+}); 
+
+//Creating logout route
+app.get('/logout',(req,res)=>{
+    res.cookie("token","");
+    res.redirect("/login");
+})
+
+//Adding middlewares for protected routes
+function isLoggedIn(req,res,next){
+    if(req.cookies.token==="") return res.send("You Must be Loggen in");
+    else{
+        let data =  jwt.verify(req.cookies.token,"secretKey")
+        req.user = data;
+    }
+    next();
+}
 app.listen(3000);
